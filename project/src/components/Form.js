8f988e345c94e77a2e608398b3d55b118936669e
jarghiskhan <jared.l.wyatt@outlook.com>
2021-07-13 21:49:20 -0400

    Form inputs are made

--- project/src/components/Form.js
+++ project/src/components/Form.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as yup from "yup";
 
 function Form() {
   const [formState, setFormState] = useState({
@@ -14,45 +15,74 @@ function Form() {
     password: "",
     terms: "",
   });
-  
+
+  const [isButtonDisabled , setIsButtonDisabled] = useState(false)
+
+  const formSchema = yup.object().shape({
+    name: yup.string().required("Needs name."),
+    email: yup.string().email("Must be a valid email address").required(),
+    password: yup
+      .string()
+      .required("No password provided.")
+      .min(8, "Password is too short - should be 8 chars minimum.")
+      .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+    terms: yup.boolean().oneOf([true], "Must accept Terms of Service."),
+  });
+
+  useEffect(() => {
+    formSchema.isValid(formState).then((valid) => {
+      console.log("valid", valid);
+      setIsButtonDisabled(!valid)
+    });
+  }, [formState]);
+
+//Form submission function
   const formSubmit = (e) => {
     e.preventDefault();
     console.log("form submitted");
   };
+
+//onChange function
   const inputChange = (e) => {
-    setFormState({ name: e.target.value });
+    const newFormData ={
+        ...formState,
+        [e.target.name]: (e.target.type === "checkbox") ? e.target.checked : e.target.value
+    }
+    setFormState(newFormData);
   };
+
+
 
   return (
     <div>
       <h1>Form</h1>
       <form onSubmit={formSubmit}>
         <p>
-          <label htmlFor="nameInput" name="name">
+          <label htmlFor="nameInput" >
             Name:<span> </span>
-            <input type="text" id="nameInput" />
+            <input type="text" id="nameInput" name="name" onChange={inputChange} value={formState.name}/>
           </label>
         </p>
 
         <p>
-          <label htmlFor="emailInput" name="email">
+          <label htmlFor="emailInput" >
             Email:<span> </span>
-            <input type="email" id="emailInput" />
+            <input type="email" id="emailInput" name="email" onChange={inputChange} value={formState.email}/>
           </label>
         </p>
         <p>
-          <label htmlFor="passwordInput" name="password">
+          <label htmlFor="passwordInput" >
             Password:<span></span>
-            <input type="password" id="passwordInput" />
+            <input type="password" id="passwordInput" name="password" onChange={inputChange} value={formState.password}/>
           </label>
         </p>
         <p>
-          <label htmlFor="terms" name="terms">
+          <label htmlFor="terms" >
             Approve Terms of Service:<span> </span>
-            <input type="checkbox" id="terms" checked={false} />
+            <input type="checkbox" id="terms" name="terms" onChange={inputChange} checked={formState.terms}/>
           </label>
         </p>
-        <button type="submit">Submit</button>
+        <button disabled={isButtonDisabled} type="submit">Submit</button>
       </form>
     </div>
   );
